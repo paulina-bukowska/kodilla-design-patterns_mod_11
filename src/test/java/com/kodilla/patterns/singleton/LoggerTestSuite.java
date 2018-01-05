dda4a6ad8a246410233111ce0b323c62608624fa
paulawaster <pbukowska16@gmail.com>
2018-01-05 20:26:36 +0100

    zadanie 11.1 - dodany drugi test

--- src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class LoggerTestSuite {
     @Test
-    public void testGetLastLog() {
+    public void testGetLastLogWhenOneLog() {
         //Given
         Logger.getInstance().log("web.log.20131115.txt");
 
@@ -14,5 +14,18 @@ public class LoggerTestSuite {
 
         //Then
         Assert.assertEquals("web.log.20131115.txt", lastLog);
+    }
+
+    @Test
+    public void testGetLastLogWhenTwoLogs() {
+        //Given
+        Logger.getInstance().log("web.log.20131115.txt");
+        Logger.getInstance().log("web.log.20131117.txt");
+
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+
+        //Then
+        Assert.assertEquals("web.log.20131117.txt", lastLog);
     }
 }
